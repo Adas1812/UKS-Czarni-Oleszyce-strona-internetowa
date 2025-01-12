6159072e79b152831a920ab797b23f007c40aece
Adas1812 <adas1812pl@Gmail.com>
2025-01-12 14:26:27 +0100

    z

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const imageNames = [
+
+function createGallery(containerId, folderPath, imageNames) {
+const gallery = document.getElementById(containerId);
+}
+
+imageNames.forEach((imageName) => {
+    const img = document.createElement("img"); 
+    img.src = folderPath + imageName;
+    gallery.appendChild(img); 
+});
+
+/* Czarni-Dachnów */
+const dachnowImages = [
     "img1.jpg",
     "img2.jpg",
     "img3.jpg",
     "img4.jpg",
     "img5.jpg",
     "img6.jpg"
-]
+];
+createGallery("gallery1", "/galeria/Czarni - Dachnów/", dachnowImages);
 
-const folderPath = "/galeria/Czarni - Dachnów/";
-
-const gallery = document.getElementById("gallery1");
-
-imageNames.forEach((imageName) => {
-    const img = document.createElement("img"); 
-    img.src = folderPath + imageName; 
-    gallery.appendChild(img); 
-});
+/* Czarni-Turniej Młodów */
+const turniejImages = [
+    "img1.jpg",
+    "img2.jpg",
+    "img3.jpg",
+    "img4.jpg",
+    "img5.jpg",
+    "img6.jpg"
+];
+createGallery("gallery2", "/galeria/Czarni - Turniej Młodów/", turniejImages);
